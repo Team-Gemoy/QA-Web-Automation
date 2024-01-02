ccf52dc30c535673c947c5c5286e3c75eac64ca1
Shidqi Adiatma <adiatma944@gmail.com>
2024-01-02 21:46:43 +0700

    initialize

---
+++ src/main/java/factories/driverManager.java
@@ -0,0 +1,29 @@
+package factories;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author Shidqi Adiatma a.k.a. hipstertester on 07/12/23
+ * @project secondhand-web-selenium-cucumber
+ */
+
+public class driverManager {
+
+    private static final driverManager instance = new driverManager();
+    ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    private driverManager() {
+    }
+
+    public static driverManager getInstance() {
+        return instance;
+    }
+
+    public WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public void setDriver(WebDriver webDriver) {
+        driver.set(webDriver);
+    }
+}
