70c9d9c7905288ed9b2ecaf816b1a5ea524893d5
Shidqi Adiatma <adiatma944@gmail.com>
2024-01-03 21:31:19 +0700

    added example test

---
+++ src/main/java/helpers/enums/fileType.java
@@ -0,0 +1,12 @@
+package helpers.enums;
+
+/**
+ * @author Shidqi Adiatma a.k.a. hipstertester on 07/12/23
+ * @project secondhand-web-selenium-cucumber
+ */
+
+public enum fileType {
+    PNG,
+    JPG,
+    JPEG
+}
